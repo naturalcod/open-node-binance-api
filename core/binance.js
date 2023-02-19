e3d0dbdf8e584d3aa72e980fe46a50e61daa3aef
Natural Code <evnikolaevv@gmail.com>
2023-02-19 20:13:00 +1000

    git-init

---
+++ core/binance.js
@@ -0,0 +1,126 @@
+const endpoints = require('../lib/endpoints')
+const request = require( 'request' );
+const crypto = require( 'crypto' );
+const { reqObject, reqObjectPost } = require('../lib/requestObject');
+const makeQueryString = require('../lib/makeQueryString');
+
+module.exports = class OpenBinance {
+
+    constructor(api_key, secret_key, options = {}){
+
+        this.options = {
+            APIKEY: api_key,
+            APISECRET: secret_key,
+            timeOffset: options.timeOffset || 0,
+            recvWindow: options.recvWindow || 5000
+        }
+
+        
+    }
+
+    // Checking that API secret is provided
+    authDataRequire(){
+        
+        if(!this.options.APIKEY) throw Error(`Open Binance Error: Invalid API key`);
+
+        if(!this.options.APISECRET) throw Error(`Open Binance Error: Invalid API Secret`);
+
+    }
+    
+
+    signedRequest(url, data, method = 'GET'){
+        
+
+        data.timestamp = new Date().getTime() + this.options.timeOffset;
+
+        let query = makeQueryString(data);
+
+        let signature = crypto.createHmac( 'sha256', this.options.APISECRET).update( query ).digest( 'hex' );
+        
+        let opt = method === "POST" ? reqObjectPost(url, data, method, this.options) : reqObject(
+            url + '?' + query + '&signature=' + signature , data, method, this.options)
+        
+        if(method == 'POST') opt.form.signature = signature;
+
+        return new Promise((res) => {
+
+            request(opt, (err, response, body) => {
+
+                if(err) throw Error(err);
+    
+                if(response.statusCode !== 200) throw Error(body);
+    
+                res(JSON.parse(body));
+    
+            })
+        })
+
+        
+        
+    }
+
+    /**
+     * Funding Wallet Data
+     * @returns {Array} - Array of funding wallet actives
+     */
+    fundingWallet = () => { this.authDataRequire(); return this.signedRequest(endpoints.sapi + 'v1/asset/get-funding-asset', {}, 'POST')}
+
+
+
+    /**
+     * Fetch trade fee
+     * @param {*} symbol - Trading pair symbol - optional
+     * @returns {Object || Array} Object or Array
+     */
+    tradeFee(symbol = undefined){
+
+        this.authDataRequire();
+        
+        if(!symbol) return this.signedRequest(endpoints.sapi + 'v1/asset/tradeFee', {}, 'GET') ;           
+
+        return new Promise((resolve, reject) => {
+
+            this.signedRequest(endpoints.sapi + 'v1/asset/tradeFee', {}, 'GET').then(response => {
+
+                let filtered = response.filter(i => i.symbol == symbol);
+
+                filtered.length == 0 ? reject(new Error('Symbol is invalid')) : resolve(filtered[0])                
+
+            })
+        })
+        
+    }
+
+    /**
+     * 
+     * @param {*} type ENUM | For example MAIN_FUNDING - Detail https://binance-docs.github.io/apidocs/spot/en/#change-log
+     * @param {*} asset - asset - example USDT
+     * @param {*} amount - amount of the asset
+     * @returns 
+     */
+    universalTransfer = (type, asset, amount) => this.signedRequest(endpoints.sapi + 'v1/asset/transfer', {type, asset, amount}, 'POST')
+
+
+
+
+    /**
+     * Submit a withdraw request.
+     * @param {String} coin - Currency to withdraw
+     * @param {String} network - Network of Currency | see avaiable list of networks by allCoinsInformation()
+     * @param {String} address 
+     * @param {Number} amount 
+     * @param {String} name - Description of withdraw ()
+     * @param {Number} walletType  - 0-spot wallet | 1-funding wallet
+     * @returns {Object}
+     */
+    withdraw = (coin, network, address, amount, name, walletType) => this.signedRequest(endpoints.sapi + 'v1/capital/withdraw/apply', {coin, network, address, amount, name : encodeURIComponent(name.trim()), walletType}, 'POST')
+
+
+    /**
+     * Get information of coins (available for deposit and withdraw) for user.
+     * @returns {Array}
+     */
+    allCoinsInfomation = () => this.signedRequest(endpoints.sapi + 'v1/capital/config/getall', {}, 'GET')
+    
+    
+}
---
+++ index.js
@@ -0,0 +1,3 @@
+const OpenBinance = require("./core/binance");
+
+module.exports = OpenBinance;
